f6bdbfeec0e03c18b6c34772817abae9500a17a4
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-07-23 23:41:31 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,12 +1,11 @@
-def using_include(array, element)
-
+using_include(array, element)
+(using_include(array, element)).to eq(true)
 end
-
-def using_sort(array)
-
+using_sort(array)
+(using_sort(array).last) .to eq( )
 end
-
 def using_reverse(array)
+
 
 end
 
